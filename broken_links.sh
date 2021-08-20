b79f58337e9c734e3021ceed90e03dd8ef30147e
Planeshifter <pburckhardt@outlook.com>
2021-08-20 12:30:41 -0400

    Append comma for warnings and refactor

--- broken_links.sh
+++ broken_links.sh
@@ -117,13 +117,13 @@ fi
 FILES=$(find $DIR -name '*.md' -type f)
 
 # Define list of broken links:
-FAILURES=""
+FAILURES="["
 
 # Define a count for the number of broken links:
 FAILURES_COUNT=0
 
 # Define list of warnings:
-WARNINGS=""
+WARNINGS="["
 
 # Define a count for the number of warnings:
 WARNINGS_COUNT=0
@@ -151,7 +151,7 @@ for FILE in $FILES; do
             ## Add the URL to the list of broken links if not already there:
             if [[ $FAILURES != *"$URL"* ]]; then
                 # Append comma if not empty:
-                if [ -n "$FAILURES" ]; then
+                if [ "$FAILURES" != "[" ]; then
                     FAILURES="$FAILURES,"
                 fi
                 FAILURES='$FAILURES { "url": $URL, "status": "$STATUS" }'
@@ -164,6 +164,10 @@ for FILE in $FILES; do
                 echo -e "Status code for $URL is $STATUS - $(status_code_to_string $STATUS) \u26A0"
                 ## Add the URL to the list of warnings if not already there:
                 if [[ $WARNINGS != *"$URL"* ]]; then
+                    # Append comma if not empty:
+                    if [ "$WARNINGS" != "[" ]; then
+                        WARNINGS="$WARNINGS,"
+                    fi
                     WARNINGS='$WARNINGS { "url": $URL, "status": "$STATUS" }'
                     WARNINGS_COUNT=$((WARNINGS_COUNT+1))
                 fi
@@ -172,11 +176,17 @@ for FILE in $FILES; do
     done
 done
 
+# Add closing bracket to the list of broken links:
+FAILURES="$FAILURES ]"
+
+# Add closing bracket to the list of warnings:
+WARNINGS="$WARNINGS ]"
+
 echo "Total number of broken links: $FAILURES_COUNT"
 echo "Total number of warnings: $WARNINGS_COUNT"
 
 # Assign the list indicating broken links to the `failures` output variable:
-echo "::set-output name=failures::$(echo '\[ $FAILURES \]' )"
+echo "::set-output name=failures::$(echo $FAILURES )"
 
 # Assign the list indicating warnings to the `warnings` output variable:
-echo "::set-output name=warnings::$(echo '\[ $WARNINGS \]' )"
+echo "::set-output name=warnings::$(echo $WARNINGS )"
