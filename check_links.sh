6466a2028d9ca153cffbdc83691bd41cc1bcc815
Philipp Burckhardt <pburckhardt@outlook.com>
2022-05-10 10:22:14 -0400

    Print tables of failures and warnings

--- check_links.sh
+++ check_links.sh
@@ -187,8 +187,16 @@ FAILURES="$FAILURES ]"
 # Add closing bracket to the list of warnings:
 WARNINGS="$WARNINGS ]"
 
-echo "Total number of broken links: $FAILURES_COUNT"
-echo "Total number of warnings: $WARNINGS_COUNT"
+echo "# Summary" >> $GITHUB_STEP_SUMMARY
+
+echo "Total number of broken links: $FAILURES_COUNT" >> $GITHUB_STEP_SUMMARY
+echo "Total number of warnings: $WARNINGS_COUNT" >> $GITHUB_STEP_SUMMARY
+
+echo "## Failures" >> $GITHUB_STEP_SUMMARY
+echo $FAILURES | md-table -c >> $GITHUB_STEP_SUMMARY
+
+echo "## Warnings" >> $GITHUB_STEP_SUMMARY
+echo $WARNINGS | md-table -c >> $GITHUB_STEP_SUMMARY
 
 # Assign the list indicating broken links to the `failures` output variable:
 echo "::set-output name=failures::'$(echo $FAILURES)'"
